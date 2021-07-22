e1e19f41c0fe472c0b05317221e0ba3640756a59
zakiwo <hiroki.ozaki39@gmail.com>
2021-07-22 07:11:13 +0000

    [finish]All Rspec Test has green

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,15 +4,16 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    @book = Book.new
+    @new_book = Book.new
   end
 
   def create
-    @book = Book.new(book_params)
-    if @book.save
+    @new_book = Book.new(book_params)
+    if @new_book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@new_book)
     else
+      @books = Book.all
       render :index
     end
   end
@@ -27,7 +28,7 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    if @book.update()
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
@@ -38,6 +39,7 @@ class BooksController < ApplicationController
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully deleted."
     redirect_to books_path
   end
 
